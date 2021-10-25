9148568fa355ccd858a59b0c94d7997ea6d70628
Mike Johnston <mike.david.johnston@gmail.com>
2021-10-24 20:52:19 -0600

    demonstrate minting unqiue nfts using hardhat

--- scripts/run.js
+++ scripts/run.js
@@ -9,6 +9,16 @@ const main = async () => {
 	await nftContract.deployed()
 	// log the address of where it has been deployed
 	console.log('Contract deployed to:', nftContract.address)
+
+	// Mint an NFT from the contract
+	let txn = await nftContract.mintAnNFT()
+	// Wait for it to be mined.
+	await txn.wait()
+
+	// Mint another NFT.
+	txn = await nftContract.mintAnNFT()
+	// Wait for it to be mined.
+	await txn.wait()
 }
 
 // give an error if the above doesn't work, else run it and close the program
